f9e0c674f0cc6155d595a7bf9f722c13849b1897
ReyhanehAhani <reyhaneh.ahani@gmail.com>
2024-08-22 18:53:32 +0330

    add routing

---
+++ website/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+def creat_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'r147217287r'
+
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+
+    return app
+
+
